a94954071b32b1915300a2eb05a236e82ad99e7e
Emilia <emiartola@gmail.com>
2022-07-08 20:22:41 -0300

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'acerca', component: AcercaDeComponent},
+  {path: 'aptitudes', component: AptitudesComponent},
+  {path: 'experiencia', component: ExperienciaComponent},
+  {path: 'educacion', component: EducacionComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { PortfolioService } from './services/portfolio.service';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
-import { ExperienciaYEducacionComponent } from './components/experiencia-y-educacion/experiencia-y-educacion.component';
-import { LogrosComponent } from './components/logros/logros.component';
+import { HomeComponent } from './components/home/home.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +18,9 @@ import { LogrosComponent } from './components/logros/logros.component';
     EncabezadoComponent,
     AcercaDeComponent,
     AptitudesComponent,
-    ExperienciaYEducacionComponent,
-    LogrosComponent
+    HomeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
